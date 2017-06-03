e9a8083fe7f48b1b2803e5ca906c77bea3c8b79e
Owais Chunawala <chunawalaowais@gmail.com>
2017-06-03 11:42:39 +0530

    Using VSC on JustJava

---
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -0,0 +1,93 @@
+
+/**
+ * IMPORTANT: Add your package below. Package name can be found in the project's AndroidManifest.xml file.
+ * This is the package name our example uses:
+ * <p>
+ * package com.example.android.justjava;
+ */
+package com.example.android.justjava;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+
+/**
+ * This app displays an order form to order coffee.
+ */
+public class MainActivity extends AppCompatActivity {
+    int quantity = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        //int quantity = 2;
+        int price = calculatePrice();
+        createOrderSummary(price);
+    }
+
+
+    /**
+     * Calculates the price of the order.
+     *
+     *  is the number of cups of coffee ordered
+     */
+    private int calculatePrice() {
+        return (quantity * 5);
+    }
+
+    public void increment(View view) {
+        //int quantity = 2;
+        quantity++;
+        displayQuantity(quantity);
+        displayPrice(quantity * 5);
+    }
+
+    public void decrement(View view) {
+        //int quantity = 2;
+        quantity--;
+        displayQuantity(quantity);
+        displayPrice(quantity * 5);
+
+    }
+    public void createOrderSummary(int price){
+        String name="Name: Kaptain Kunal\n";
+        String quant = "Quantity: " + quantity + "\n";
+        String priceMessage = "Total = $" + price + "\nThank You!\n" ;
+        displayMessage(name + quant+ priceMessage);
+
+    }
+
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void displayQuantity(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int displayQuantity) {
+        TextView priceTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(displayQuantity));
+    }
+
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(message);
+    }
+}

ad06d059d7c7b3bbad53a8077669060f99505b69
Owais Chunawala <chunawalaowais@gmail.com>
2017-06-03 21:19:14 +0530

    EditText, inputType and getText(). Tried & tested

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
@@ -60,7 +61,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
     public void createOrderSummary(int price){
-        String name="Name: Kaptain Kunal\n";
+        String name="Your name is "+ displayName()+ "\n";
         String whippedCreamString = "Add whipped cream? :" + displayBoolWipCream()+ "\n";
         String chocoString = "Add whipped cream? :" + displayBoolChoco()+ "\n";
         String quant = "Quantity: " + quantity + "\n";
@@ -105,5 +106,9 @@ public class MainActivity extends AppCompatActivity {
         CheckBox ChocolateCB = (CheckBox) findViewById(R.id.Chocolate_CB);
         Boolean checkedState = ChocolateCB.isChecked();
         return checkedState;
+    }
+    private String displayName(){
+        EditText name =  (EditText) findViewById(R.id.name_edit_text);
+        return name.getText().toString();
     }
 }
